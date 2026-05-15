dfd94eb4ec44d270a252690bc837ed83809ef343
João Silva <joaoandre.m.silva@gmail.com>
2026-05-15 17:24:48 +0100

    Ported code to display the card list in english from TCG 1 (currently looks broken without the translated text).

--- src/engine/bank02.asm
+++ src/engine/bank02.asm
@@ -6922,17 +6922,15 @@ AppendOwnedCardCountNumber:
 .end
 	call GetOwnedCardCount
 	call ConvertToNumericalDigits
-	ldfw bc, "枚"
-	ld [hl], c
-	inc hl
-	ld [hl], b
-	inc hl
 	ld [hl], $00 ; insert byte terminator
 	pop hl
 	pop de
 	pop bc
 	pop af
 	ret
+REPT $7 ; dummy NOPs here to pad space freed
+	nop
+ENDR
 
 ; print header info (card count and player name)
 PrintPlayersCardsHeaderInfo:
@@ -6996,17 +6994,15 @@ PrintTotalNumberOfCardsInCollection:
 	call .PlaceNumericalChar
 	call .PlaceNumericalChar
 	call .PlaceNumericalChar
-	ldfw bc, "枚"
-	ld [hl], c ; 枚
-	inc hl
-	ld [hl], b
-	inc hl
 	ld [hl], TX_END
 	lb de, 13, 0
 	call InitTextPrinting
 	ld hl, wTempCardCollection
 	call ProcessText
 	ret
+REPT $7 ; dummy NOPs here to pad space freed
+	nop
+ENDR
 
 ; places a numerical character in hl from de
 ; doesn't place a 0 if no non-0
